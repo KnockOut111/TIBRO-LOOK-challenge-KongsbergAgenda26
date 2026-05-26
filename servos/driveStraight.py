60f14dc83330969953ce6041d878956864594d54
Fosen1 <ffosen@hotmail.com>
2026-05-26 22:43:37 +0200

    Corrected values for servos to work as intended.

--- servos/driveStraight.py
+++ servos/driveStraight.py
@@ -8,11 +8,11 @@ kit = ServoKit(channels=16)
 left_engines = [kit.continuous_servo[i] for i in range(3)]
 right_engines = [kit.continuous_servo[i] for i in range(3, 6)]
 
-STOP = 0
-LEFT_FORWARD = -1.0
-LEFT_BACKWARD = 1.0
-RIGHT_FORWARD = 1.0
-RIGHT_BACKWARD = -1.0
+STOP = -1
+LEFT_FORWARD = 0
+LEFT_BACKWARD = 1
+RIGHT_FORWARD = 1
+RIGHT_BACKWARD = 0
 
 def set_drive(left_speed, right_speed):
     for engine in left_engines:
