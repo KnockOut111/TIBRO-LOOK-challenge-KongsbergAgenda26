08a6a0e11d94dca1913d17c7a823eed280951a54
Knut Olav <knut.olav.kragh@hotmail.no>
2026-05-26 22:17:22 +0200

    Made a calibrate script for the servos. Also changed the driveStraight.py code using continous_servo function from the ServoKit package.

---
+++ servos/calibrateServos.py
@@ -0,0 +1,18 @@
+# One servo at a time.
+# Check that they respond to throttle input. 
+# Check that they stop when throttle is set to 0.0. 
+# Check that they reverse when throttle is negative. 
+# Check that they go forward when throttle is positive.
+
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=16)
+
+while True:
+    ch = int(input("Channel 0-5: "))
+    throttle = float(input("Throttle -1.0 to 1.0, 0 stop: "))
+
+    if 0 <= ch <= 5 and -1.0 <= throttle <= 1.0:
+        kit.continuous_servo[ch].throttle = throttle
+    else:
+        print("Invalid input")
--- servos/driveStraight.py
+++ servos/driveStraight.py
@@ -5,44 +5,37 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-left_engines = [kit.servo[i] for i in range(3)]      # 0,1,2
-right_engines = [kit.servo[i] for i in range(3, 6)]  # 3,4,5
+left_engines = [kit.continuous_servo[i] for i in range(3)]
+right_engines = [kit.continuous_servo[i] for i in range(3, 6)]
 
-STOP = 90
-LEFT_FORWARD = 0
-LEFT_BACKWARD = 180
-RIGHT_FORWARD = 180
-RIGHT_BACKWARD = 0
-
+STOP = 0
+LEFT_FORWARD = -1.0
+LEFT_BACKWARD = 1.0
+RIGHT_FORWARD = 1.0
+RIGHT_BACKWARD = -1.0
 
 def set_drive(left_speed, right_speed):
     for engine in left_engines:
-        engine.angle = left_speed
+        engine.throttle = left_speed
     for engine in right_engines:
-        engine.angle = right_speed
+        engine.throttle = right_speed
 
 set_drive(STOP, STOP)
 
 try:
-
     while True:
         command = input("Enter command (forward/backward/stop/quit): ").strip().lower()
-        
 
         if command == "forward":
-            print("Driving forward")
             set_drive(LEFT_FORWARD, RIGHT_FORWARD)
 
         elif command == "backward":
-            print("Driving backward")
             set_drive(LEFT_BACKWARD, RIGHT_BACKWARD)
 
         elif command == "stop":
-            print("Stopping")
             set_drive(STOP, STOP)
 
         elif command == "quit":
-            print("Exiting")
             set_drive(STOP, STOP)
             break
 
@@ -50,5 +43,4 @@ try:
             print("Unknown command")
 
 except KeyboardInterrupt:
-    print("\nEmergency stop")
     set_drive(STOP, STOP)
