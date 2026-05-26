ff761122cdc9699868541c5571318f89e738146a
Knut Olav <knut.olav.kragh@hotmail.no>
2026-05-26 21:13:52 +0200

    Made some changes to the driveStraight code so that command forward and stop may work.

--- servos/driveStraight.py
+++ servos/driveStraight.py
@@ -13,18 +13,18 @@ servo4 = kit.servo[4]
 servo5 = kit.servo[5]
 
 #For locomotion servos
-servo6 = kit.servo[6]
-servo7 = kit.servo[7]
-servo8 = kit.servo[8]
-servo9 = kit.servo[9]
-servo10 = kit.servo[10]
-servo11 = kit.servo[11]
+# servo6 = kit.servo[6]
+# servo7 = kit.servo[7]
+# servo8 = kit.servo[8]
+# servo9 = kit.servo[9]
+# servo10 = kit.servo[10]
+# servo11 = kit.servo[11]
 
 
 print("Moving forward script started... ")
 print("Enter command: " + command)
 
-while command != "stop":
+while command == "forward":
     print("Engine1 drives 360 deg forward... ")
     servo0.angle = 360
     time.sleep(0.1)
@@ -48,3 +48,13 @@ while command != "stop":
     print("Engine6 drives 360 deg forward... ")
     servo5.angle = 360
     time.sleep(0.1)
+
+    if command == "stop":
+        print("Stopping all engines... ")
+        servo0.angle = 90
+        servo1.angle = 90
+        servo2.angle = 90
+        servo3.angle = 90
+        servo4.angle = 90
+        servo5.angle = 90
+        break       
