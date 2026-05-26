cda8dd736633dac94bf07b7f0416da42c96570ce
Knut Olav <knut.olav.kragh@hotmail.no>
2026-05-26 21:22:23 +0200

    Changed code to work better with commands.

--- servos/driveStraight.py
+++ servos/driveStraight.py
@@ -1,60 +1,99 @@
+# from adafruit_servokit import ServoKit
+# import time
+
+# kit = ServoKit(channels=16)
+# command = str(input("Enter command: "))
+
+# #Only for main engines for forword and backward motion = kit.servo[0]
+# servo0 = kit.servo[0]
+# servo1 = kit.servo[1]
+# servo2 = kit.servo[2]
+# servo3 = kit.servo[3]
+# servo4 = kit.servo[4]
+# servo5 = kit.servo[5]
+
+# #For locomotion servos
+# # servo6 = kit.servo[6]
+# # servo7 = kit.servo[7]
+# # servo8 = kit.servo[8]
+# # servo9 = kit.servo[9]
+# # servo10 = kit.servo[10]
+# # servo11 = kit.servo[11]
+
+
+# print("Moving forward script started... ")
+# print("Enter command: " + command)
+
+# while command == "forward":
+#     print("Engine1 drives 360 deg forward... ")
+#     servo0.angle = 0-180
+#     time.sleep(0.1)
+
+#     print("Engine2 drives 360 deg forward... ")
+#     servo1.angle = 360
+#     time.sleep(0.1)
+
+#     print("Engine3 drives 360 deg forward... ")
+#     servo2.angle = 360
+#     time.sleep(0.1)
+
+#     print("Engine4 drives 360 deg forward... ")
+#     servo3.angle = 360
+#     time.sleep(0.1)
+
+#     print("Engine5 drives 360 deg forward... ")
+#     servo4.angle = 360
+#     time.sleep(0.1)
+
+#     print("Engine6 drives 360 deg forward... ")
+#     servo5.angle = 360
+#     time.sleep(0.1)
+
+#     if command == "stop":
+#         print("Stopping all engines... ")
+#         servo0.angle = 90
+#         servo1.angle = 90
+#         servo2.angle = 90
+#         servo3.angle = 90
+#         servo4.angle = 90
+#         servo5.angle = 90
+#         break       
+
 from adafruit_servokit import ServoKit
-import time
 
 kit = ServoKit(channels=16)
-command = str(input("Enter command: "))
 
-#Only for main engines for forword and backward motion = kit.servo[0]
-servo0 = kit.servo[0]
-servo1 = kit.servo[1]
-servo2 = kit.servo[2]
-servo3 = kit.servo[3]
-servo4 = kit.servo[4]
-servo5 = kit.servo[5]
+engines = [kit.servo[i] for i in range(6)]
 
-#For locomotion servos
-# servo6 = kit.servo[6]
-# servo7 = kit.servo[7]
-# servo8 = kit.servo[8]
-# servo9 = kit.servo[9]
-# servo10 = kit.servo[10]
-# servo11 = kit.servo[11]
+STOP = 90
+FORWARD = 180
+BACKWARD = 0
 
+def set_all(speed):
+    for engine in engines:
+        engine.angle = speed
 
-print("Moving forward script started... ")
-print("Enter command: " + command)
+while True:
+    command = input(
+        "Enter command (forward/backward/stop/quit): "
+    ).strip().lower()
 
-while command == "forward":
-    print("Engine1 drives 360 deg forward... ")
-    servo0.angle = 360
-    time.sleep(0.1)
+    if command == "forward":
+        print("Driving forward")
+        set_all(FORWARD)
 
-    print("Engine2 drives 360 deg forward... ")
-    servo1.angle = 360
-    time.sleep(0.1)
+    elif command == "backward":
+        print("Driving backward")
+        set_all(BACKWARD)
 
-    print("Engine3 drives 360 deg forward... ")
-    servo2.angle = 360
-    time.sleep(0.1)
+    elif command == "stop":
+        print("Stopping")
+        set_all(STOP)
 
-    print("Engine4 drives 360 deg forward... ")
-    servo3.angle = 360
-    time.sleep(0.1)
+    elif command == "quit":
+        print("Exiting")
+        set_all(STOP)
+        break
 
-    print("Engine5 drives 360 deg forward... ")
-    servo4.angle = 360
-    time.sleep(0.1)
-
-    print("Engine6 drives 360 deg forward... ")
-    servo5.angle = 360
-    time.sleep(0.1)
-
-    if command == "stop":
-        print("Stopping all engines... ")
-        servo0.angle = 90
-        servo1.angle = 90
-        servo2.angle = 90
-        servo3.angle = 90
-        servo4.angle = 90
-        servo5.angle = 90
-        break       
+    else:
+        print("Unknown command")
