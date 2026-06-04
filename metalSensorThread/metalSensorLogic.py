0ff0d7d300565ea236b219e372a260f0757ddd3d
Knut Olav <knut.olav.kragh@hotmail.no>
2026-06-04 14:28:01 +0200

    Fixed an error in spelling!

--- metalSensorThread/metalSensorLogic.py
+++ metalSensorThread/metalSensorLogic.py
@@ -28,7 +28,7 @@ class MetalSensorNode(Node):
 		super().__init__("metal_sensor_node")
 
 		#Defining a callback for the shutdown topic 
-		self.create_subscribtion( 
+		self.create_subscription( 
 			String,
             "/rover/system_shutdown",
             self.shutdown_callback,
