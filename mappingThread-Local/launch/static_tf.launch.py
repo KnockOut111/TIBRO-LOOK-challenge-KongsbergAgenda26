cd8bebbe0ecf1d67dab6824d066aa9f253d6ad73
fredrswa <fredrswa@stud.ntnu.no>
2026-06-07 22:11:17 +0200

    added layout for two IMU's

--- mappingThread-Local/launch/static_tf.launch.py
+++ mappingThread-Local/launch/static_tf.launch.py
@@ -19,10 +19,21 @@ def generate_launch_description():
             package="tf2_ros",
             executable="static_transform_publisher",
             arguments=[
-                "0.0", "0.0", "0.10",
+                "0.05", "0.0", "0.10",
                 "0.0", "0.0", "0.0",
                 "base_link",
-                "imu_link",
+                "imu_front_link",
+            ],
+        ),
+
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=[
+                "-0.05", "0.0", "0.10",
+                "0.0", "0.0", "0.0",
+                "base_link",
+                "imu_rear_link",
             ],
         ),
     ])
